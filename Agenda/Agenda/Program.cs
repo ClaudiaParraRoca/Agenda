d1ddaca53b37744ca264a6ad722397feff520238
Oriol <orioldeprius@gmail.com>
2024-01-24 21:24:36 +0100

    Menu Agenda creat

--- Agenda/Agenda/Program.cs
+++ Agenda/Agenda/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MenuPrincipal();
+            ValidarOpcio();
+        }
+
+        static void MenuPrincipal()
+        {
+            string opcio = "╔═══════════════════════════╗\n" +
+                           "║           Agenda          ║\n" +
+                           "╠═══════════════════════════╣\n" +
+                           "║1. Donar d'alta a l'usuari ║\n" +
+                           "║2. Recuperar usuari        ║\n" +
+                           "║3. Modificar usuari        ║\n" +
+                           "║4. Eliminar usuari         ║\n" +
+                           "║5. Mostrar agenda          ║\n" +
+                           "║6. ordenar agenda          ║\n" +
+                           "║Q. Sortir de l'agenda      ║\n" +
+                           "╚═══════════════════════════╝\n";
+            Console.WriteLine(opcio);
+        }
+        static void ValidarOpcio()
+        {
+            Console.Write("Introdueix una opcio --> ");
         }
     }
 }
